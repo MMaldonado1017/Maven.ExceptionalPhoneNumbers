8745b07cfb9e8e7d29325ab5c5ef792a13cb94e4
MMaldonado1017 <miguelmaldonado199@gmail.com>
2023-07-09 13:54:38 -0400

    completed

--- src/main/java/com/zipcodewilmington/phone/PhoneNumber.java
+++ src/main/java/com/zipcodewilmington/phone/PhoneNumber.java
@@ -2,16 +2,15 @@ package com.zipcodewilmington.phone;
 
 import com.zipcodewilmington.exceptions.InvalidPhoneNumberFormatException;
 
+import java.util.Random;
+import java.util.logging.Logger;
+
 /**
  * Created by leon on 5/10/17.
  */
 public final class PhoneNumber {
     private final String phoneNumberString;
-
-    // default constructor is uncallable
-    private PhoneNumber() throws InvalidPhoneNumberFormatException {
-        this(null);
-    }
+    private static final Logger LOGGER = Logger.getLogger(PhoneNumber.class.getName());
 
     // non-default constructor is package-protected
     protected PhoneNumber(String phoneNumber) throws InvalidPhoneNumberFormatException {
@@ -20,6 +19,38 @@ public final class PhoneNumber {
             throw new InvalidPhoneNumberFormatException();
         }
         this.phoneNumberString = phoneNumber;
+    }
+
+    public static PhoneNumber createPhoneNumber(String phoneNumber) throws InvalidPhoneNumberFormatException {
+        LOGGER.info("Attempting to create a new PhoneNumber object with a value of " + phoneNumber);
+        return new PhoneNumber(phoneNumber);
+    }
+
+    public static PhoneNumber createPhoneNumberSafely(int areaCode, int centralOfficeCode, int phoneLineCode) {
+        String phoneNumber = "(" + areaCode + ")-" + centralOfficeCode + "-" + phoneLineCode;
+        try {
+            return createPhoneNumber(phoneNumber);
+        } catch (InvalidPhoneNumberFormatException e) {
+            LOGGER.warning(phoneNumber + " is not a valid phone number");
+            return null;
+        }
+    }
+
+    public static PhoneNumber createRandomPhoneNumber() {
+        Random rand = new Random();
+        int areaCode = rand.nextInt(900) + 100; // to generate 3 digit number
+        int centralOfficeCode = rand.nextInt(900) + 100;
+        int phoneLineCode = rand.nextInt(9000) + 1000; // to generate 4 digit number
+
+        return createPhoneNumberSafely(areaCode, centralOfficeCode, phoneLineCode);
+    }
+
+    public static PhoneNumber[] createRandomPhoneNumberArray(int count) {
+        PhoneNumber[] phoneNumbers = new PhoneNumber[count];
+        for (int i = 0; i < count; i++) {
+            phoneNumbers[i] = createRandomPhoneNumber();
+        }
+        return phoneNumbers;
     }
 
     public String getAreaCode() {
@@ -32,7 +63,7 @@ public final class PhoneNumber {
 
     public String getPhoneLineCode() {
         return toString().substring(10, 14);
-}
+    }
 
     @Override
     public String toString() {
